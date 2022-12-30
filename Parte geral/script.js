f5eece83e5a6855928ca603524cee215cece44f5
Darlison <Drsilva123jd@gmail.com>
2022-12-30 17:02:14 -0300

    Projeto grid

---
+++ Parte geral/script.js
@@ -0,0 +1,31 @@
+
+function comprar(){
+    window.document.getElementById('item')
+    window.location = "./comprar.html";
+}
+
+function voltar(){
+    window.document.getElementById('voltar')
+    window.location='./index.html';
+}
+
+
+function facebook(){
+    window.document.querySelector('i')
+    window.location= "https://www.facebook.com/drums.souza.9";
+}
+
+function whatsapp(){
+    window.document.querySelector('i')
+    window.location= "https://wa.me/<5511954201792?text=Olá,20%gostaria20%de20%algumas20%informações.20%Você20%pode20%me20%ajudar?";
+}
+
+function instagram(){
+    window.document.querySelector('i')
+    window.location= "https://instagram.com/drums.souza.9";
+}
+
+function linkedin(){
+    window.document.querySelector('i')
+    window.location= "https://www.linkedin.com/in/darlison-silva-86113a191";
+}
