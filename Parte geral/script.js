3e9f105f511dbd20caa95998985504c524908841
Darlison <Drsilva123jd@gmail.com>
2022-12-31 16:19:34 -0300

    Second project

--- Parte geral/script.js
+++ Parte geral/script.js
@@ -1,31 +1,15 @@
 
 function comprar(){
     window.document.getElementById('item')
-    window.location = "./comprar.html";
+    window.location = "./compra.html";
 }
 
 function voltar(){
     window.document.getElementById('voltar')
-    window.location='./index.html';
-}
-
-
-function facebook(){
-    window.document.querySelector('i')
-    window.location= "https://www.facebook.com/drums.souza.9";
+    window.location='./adb.html';
 }
 
 function whatsapp(){
     window.document.querySelector('i')
-    window.location= "https://wa.me/<5511954201792?text=Olá,20%gostaria20%de20%algumas20%informações.20%Você20%pode20%me20%ajudar?";
-}
-
-function instagram(){
-    window.document.querySelector('i')
-    window.location= "https://instagram.com/drums.souza.9";
-}
-
-function linkedin(){
-    window.document.querySelector('i')
-    window.location= "https://www.linkedin.com/in/darlison-silva-86113a191";
+    window.location= "https://wa.me/<5511954201792?text=Olá, Darlison Silva. Passando para te avisar que visualizei o seu site.";
 }
